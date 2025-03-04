94dd5ba57e36a52e5e16b27718a92586f5b51293
Thalita Brice Alves Costa <thalitabac@gmail.com>
2025-03-03 23:08:19 -0300

    Atualização do projeto - mudanças no index.js e novas páginas

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 import express from "express";
 import autenticar from "./seguranca/autenticar.js";
+import session from "express-session";
 
 const porta = 3000;
 const localhost = "0.0.0.0"; //define que o nosso aplicativo estará disponível em todas as interfaces de redes deste computador
 
 const app = express();
 
+app.use(session({
+    secret: "m1Nh4Ch4v3S3cR3t4",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 *15
+    }
+}))
+//oferecer recurso login
+app.get("/login", (requisicao, resposta) =>{
+    resposta.redirect('/login.html');
+})
+
+app.post("/login", (requisicao, resposta) => {
+    const usuario = requisicao.body.usuario;
+    const senha = requisicao.body.senha;
+    if(usuario === "admin" && senha === "admin"){
+        requisicao.session.autenticado = true;
+        resposta.redirect('/menu.html');
+    } else {
+        resposta.redirect('/login.html');
+    }
+})
 //prepara o servidor para disponibilizar recursos estáticos
 app.use(express.static("./publico"));
 
